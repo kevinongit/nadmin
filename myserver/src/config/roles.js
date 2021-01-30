5de0e51f4cd0446b23f71cb20760755d010b7ea1
Kevin <nait90@gmail.com>
2021-01-30 18:18:23 +0900

    role changed : none, user, admin

--- myserver/src/config/roles.js
+++ myserver/src/config/roles.js
@@ -1,11 +1,10 @@
 // const roles = ['user', 'admin']
-const roles = ['none', 'clerk', 'cashier', 'manager'];
+const roles = ['none', 'user', 'admin'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], []);
-roleRights.set(roles[1], []);
-roleRights.set(roles[2], []);
-roleRights.set(roles[3], ['getUsers', 'manageUsers']);
+roleRights.set(roles[1], 'owner');
+roleRights.set(roles[2], ['getUsers', 'manageUsers']);
 
 module.exports = {
   roles,
